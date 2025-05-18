b4516fe0e28d0997ce6ea3c9ef54238c87fa3a3b
yatharth-kaushik <yatharth1820kaushik@gmail.com>
2025-05-18 18:54:38 +0530

    Create Given number is three digit or not.c

---
+++ Given number is three digit or not/Given number is three digit or not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int num;
+printf("Enter the number :- ");
+scanf("%d",&num);
+if (num>99 && num<1000)
+{
+    printf("The Given number is a 3 digit number !!");
+}
+else{
+    printf("The Given number is not a 3 digit number !!");
+}
+
+}
