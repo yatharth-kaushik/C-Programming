0dfde398bc00a691cab4a02b239e766c968739af
yatharth-kaushik <yatharth1820kaushik@gmail.com>
2025-05-18 18:48:35 +0530

    Create EvenOrOdd.c

---
+++ Given number is even or odd/EvenOrOdd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number :- ");
+    scanf("%d",&num);
+    if (num%2==0)
+    {
+        printf("Given number is even !!");
+    }
+    else{
+        printf("Given number is odd !!");
+    }
+    
+}
