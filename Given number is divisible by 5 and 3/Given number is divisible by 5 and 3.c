9d797f44c0b21116687f4a50e8850e123d278fdb
yatharth-kaushik <yatharth1820kaushik@gmail.com>
2025-05-18 18:55:49 +0530

    Create Given number is divisible by 5 and 3.c

---
+++ Given number is divisible by 5 and 3/Given number is divisible by 5 and 3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number :- ");
+    scanf("%d",&num);
+    if (num%5==0 && num%3==0)
+    {
+        printf("Given number is divisible by 5 and 3!!");
+    }
+    else{
+        printf("Given number is not divisible by 5 and 3!!");
+    }
+    
+}
